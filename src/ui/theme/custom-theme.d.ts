2bdf973751bc3febe95cc9c2fba612c8c54f33c0
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-10-05 15:20:28 +0200

    feat(components-init): improve theme and add utils

--- src/ui/theme/custom-theme.d.ts
+++ src/ui/theme/custom-theme.d.ts
@@ -1,12 +1,20 @@
 import "@mui/material/styles";
 
 declare module "@mui/material/styles" {
+    //interface redundant to not disable eslint no-empty-interface
     interface CustomTheme {
         variables: {
             neutral: string;
         };
     }
-
-    interface Theme extends CustomTheme {}
-    interface ThemeOptions extends CustomTheme {}
+    interface Theme {
+        variables: {
+            neutral: string;
+        };
+    }
+    interface ThemeOptions {
+        variables: {
+            neutral: string;
+        };
+    }
 }
