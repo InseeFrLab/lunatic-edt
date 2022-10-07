3bcdc2a031cf91c99327958b0d915cf52a55ae2c
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-10-07 15:09:17 +0200

    feat(components-init): add font, use it in theme

--- src/ui/theme/theme.tsx
+++ src/ui/theme/theme.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import { ThemeProvider as MuiProvider, createTheme } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
+import MarianneRegularWoff2 from "../assets/fonts";
 
 const theme = createTheme({
     variables: {
@@ -30,6 +31,23 @@ const theme = createTheme({
         },
         action: {
             hover: "#5C6F99",
+        },
+    },
+    typography: {
+        fontFamily: "Marianne, sans-serif",
+    },
+    components: {
+        MuiCssBaseline: {
+            styleOverrides: `
+            @font-face {
+              font-family: 'Marianne';
+              font-style: normal;
+              font-display: swap;
+              font-weight: 400;
+              src: local('Marianne'), local('Marianne-Regular'), url(${MarianneRegularWoff2}) format('woff2');
+              unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
+            }
+          `,
         },
     },
 });
