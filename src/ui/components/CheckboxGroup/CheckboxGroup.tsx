663b29f419d6c5febc585cd2320843536aa3802e
Paul BOURGEAIS <paul.bourgeais@keyconsulting.fr>
2022-10-07 09:28:04 +0200

    feat(checkboxGroup): replace togglebuttons, complete theme

--- src/ui/components/CheckboxGroup/CheckboxGroup.tsx
+++ src/ui/components/CheckboxGroup/CheckboxGroup.tsx
@@ -1,72 +1,63 @@
-import { ToggleButton, ToggleButtonGroup } from "@mui/material";
+import { Checkbox, Paper, Typography } from "@mui/material";
 import { CheckboxGroupOption } from "interface/CheckboxGroupOptions";
 import React from "react";
 import { memo } from "react";
 import { makeStyles } from "tss-react/mui";
-import { important } from "../../utils/utils";
 
 export type CheckboxGroupProps = {
     handleChange(response: { [name: string]: string }, value: boolean): void;
     id?: string;
-    label?: string;
     options: CheckboxGroupOption[];
     value: { [key: string]: boolean };
-    className?: string;
 };
 
 const CheckboxGroup = memo((props: CheckboxGroupProps) => {
     console.log("CheckboxGroup");
     console.log(props);
-    const { id, value, label, options, className, handleChange } = props;
+    const { id, value, options, handleChange } = props;
 
     const { classes } = useStyles();
 
-    const optionsValues: string[] = options.map(option => option.response.name);
-    const [currentOptions, setCurrentOptions] = React.useState<string[]>(() => optionsValues);
+    const handleOptions = (event: any) => {
+        // update value with the opposite of its current value
+        value[event.target.value] = !value[event.target.value];
+        handleChange({ name: event.target.value }, value[event.target.value]);
+    };
 
-const handleOptions = (event: any, newOptions: string[]) => {
-    setCurrentOptions(newOptions);
-    // update value with the opposite of its current value
-    value[event.target.value] = !value[event.target.value];
-    handleChange({ name: event.target.value }, value[event.target.value]);
-};
+    return (
+        <div id={id}>
+            {options.map(option => (
+                <Paper className={classes.root} elevation={0}>
+                    <div style={{ display: "flex" }}>
+                        {/* TODO : replace when we know in which way we send icons label */}
+                        <span>IC&nbsp;&nbsp;</span>
+                        <Typography color="textSecondary">{option.label}</Typography>
+                    </div>
 
-return (
-    <ToggleButtonGroup
-        orientation="vertical"
-        value={currentOptions}
-        onChange={handleOptions}
-        id={id}
-        aria-label={label}
-        className={className}
-    >
-        {options.map(option => (
-            <ToggleButton
-                className={classes.MuiToggleButton}
-                key={option.id}
-                selected={value[option.response.name] ?? false}
-                value={option.response.name}
-            >
-                {option.label}
-            </ToggleButton>
-        ))}
-    </ToggleButtonGroup>
-);
+                    <Checkbox
+                        key={option.id}
+                        checked={value[option.response.name] ?? false}
+                        value={option.response.name}
+                        onChange={handleOptions}
+                        className={classes.MuiCheckbox}
+                    />
+                </Paper>
+            ))}
+        </div>
+    );
 });
 
 const useStyles = makeStyles({ "name": { CheckboxGroup } })(theme => ({
-    "MuiToggleButton": {
-        marginBottom: "0.5rem",
-        border: important("2px solid #FFFFFF"),
-        borderRadius: important("6px"),
-        backgroundColor: "#FFFFFF",
-        color: theme.palette.primary.main,
-        "&.Mui-selected": {
-            borderColor: important(theme.palette.primary.main),
-            fontWeight: "bold",
-            backgroundColor: "#FFFFFF",
-            color: theme.palette.primary.main,
-        },
+    root: {
+        maxWidth: "100%",
+        margin: "1rem",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingLeft: "0.5rem",
+    },
+    MuiCheckbox: {
+        color: theme.variables.neutral,
     },
 }));
 
