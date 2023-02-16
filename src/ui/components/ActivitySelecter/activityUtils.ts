d2f4723f69bf2a772cc8134aedff5f13b3538002
laila abjil <laila.abjil@keyconsulting.fr>
2023-02-16 17:37:29 +0100

    fix(sonar): refacto activity selecter for cognitive complexity issue

--- src/ui/components/ActivitySelecter/activityUtils.ts
+++ src/ui/components/ActivitySelecter/activityUtils.ts
@@ -80,21 +80,27 @@ const getItemFromSearchResult = (
  * When selectionne activity with categories
  */
 export const processActivityCategory = (
+    value: { [key: string]: string | boolean },
     parsedValue: SelectedActivity,
     categoriesAndActivitesNomenclature: NomenclatureActivityOption[],
     setSelectedId: (id: string | undefined) => void,
     setSelectedCategories: (cats: NomenclatureActivityOption[]) => void,
 ) => {
-    setSelectedId(parsedValue.id);
-    const res = findItemInCategoriesNomenclature(parsedValue.id, categoriesAndActivitesNomenclature);
-    const resParent = getParentFromSearchResult(res);
-    const resItem = getItemFromSearchResult(res);
-    const isFullyCompleted: boolean = parsedValue.isFullyCompleted;
+    const hasId: boolean = parsedValue.id != null;
+    const hasLabel: boolean = parsedValue.label != null;
 
-    if (isFullyCompleted) {
-        setSelectedCategories(resParent);
-    } else {
-        setSelectedCategories(resItem);
+    if (hasId && !hasLabel && value && categoriesAndActivitesNomenclature) {
+        setSelectedId(parsedValue.id);
+        const res = findItemInCategoriesNomenclature(parsedValue.id, categoriesAndActivitesNomenclature);
+        const resParent = getParentFromSearchResult(res);
+        const resItem = getItemFromSearchResult(res);
+        const isFullyCompleted: boolean = parsedValue.isFullyCompleted;
+
+        if (isFullyCompleted) {
+            setSelectedCategories(resParent);
+        } else {
+            setSelectedCategories(resItem);
+        }
     }
 };
 
@@ -102,31 +108,98 @@ export const processActivityCategory = (
  * When search activity
  */
 export const processActivityAutocomplete = (
+    value: { [key: string]: string | boolean },
     parsedValue: SelectedActivity,
     setFullScreenComponent: (screen: FullScreenComponent) => void,
     setSelectedSuggesterId: (id: string | undefined) => void,
 ) => {
-    setFullScreenComponent(FullScreenComponent.ClickableListComp);
-    setSelectedSuggesterId(parsedValue.suggesterId);
+    const hasSuggesterId: boolean = parsedValue.suggesterId != null;
+    if (hasSuggesterId && value) {
+        setFullScreenComponent(FullScreenComponent.ClickableListComp);
+        setSelectedSuggesterId(parsedValue.suggesterId);
+    }
 };
 
 /**
  * When selectionne option new activity
  */
 export const processNewActivity = (
+    value: { [key: string]: string | boolean },
     parsedValue: SelectedActivity,
     categoriesAndActivitesNomenclature: NomenclatureActivityOption[],
     setFullScreenComponent: (screen: FullScreenComponent) => void,
     setCreateActivityValue: (val: string | undefined) => void,
     setSelectedCategories: (cats: NomenclatureActivityOption[]) => void,
 ) => {
-    setFullScreenComponent(FullScreenComponent.FreeInput);
-    setCreateActivityValue(parsedValue.label);
-    const hasId: boolean = parsedValue.id != null;
-
-    if (categoriesAndActivitesNomenclature && hasId) {
-        const res = findItemInCategoriesNomenclature(parsedValue.id, categoriesAndActivitesNomenclature);
-        const resItem = res?.item ? [res?.item] : [];
-        setSelectedCategories(resItem);
+    const hasLabel: boolean = parsedValue.label != null;
+    if (hasLabel && value && categoriesAndActivitesNomenclature) {
+        setFullScreenComponent(FullScreenComponent.FreeInput);
+        setCreateActivityValue(parsedValue.label);
+        const hasId: boolean = parsedValue.id != null;
+        if (hasId) {
+            const res = findItemInCategoriesNomenclature(
+                parsedValue.id,
+                categoriesAndActivitesNomenclature,
+            );
+            const resItem = res?.item ? [res?.item] : [];
+            setSelectedCategories(resItem);
+        }
     }
+};
+
+/**
+ * Find category of rank 1 when selectionne categories of rank 2 or 3
+ */
+export const findRank1Category = (
+    parsedValue: SelectedActivity,
+    categoriesAndActivitesNomenclature: NomenclatureActivityOption[],
+) => {
+    const idSelected = parsedValue.id;
+    const isFullyCompleted = parsedValue.isFullyCompleted;
+    let category = undefined;
+    //if category of 3nd rank, get category 2n rank, other get category 1r rank
+    const categorySecondRank = findItemInCategoriesNomenclature(
+        idSelected,
+        categoriesAndActivitesNomenclature,
+    );
+    //if category of 2nd rank, get category 1r rank, other undefined
+    const categoryFirstRank = findItemInCategoriesNomenclature(
+        categorySecondRank?.parent?.id,
+        categoriesAndActivitesNomenclature,
+    );
+
+    category = categoryFirstRank?.parent == null ? categorySecondRank : categoryFirstRank;
+
+    return isFullyCompleted ? category?.parent : category?.item;
+};
+
+export const selectSubCategory = (
+    selection: NomenclatureActivityOption,
+    selectedId: string | undefined,
+    selectedCategories: NomenclatureActivityOption[],
+    setSelectedCategories: (activities: NomenclatureActivityOption[]) => void,
+    onChange: (isFullyCompleted: boolean, id?: string, suggesterId?: string, label?: string) => void,
+) => {
+    const id = selectedId == selection.id ? undefined : selection.id;
+    const temp = [...selectedCategories];
+    temp.push(selection);
+    setSelectedCategories(temp);
+    onChange(false, id, undefined, undefined);
+};
+
+export const selectFinalCategory = (
+    selection: NomenclatureActivityOption,
+    selectedId: string | undefined,
+    labelOfSelectedId: string | undefined,
+    setSelectedId: (id?: string) => void,
+    setLabelOfSelectedId: (label?: string) => void,
+    onChange: (isFullyCompleted: boolean, id?: string, suggesterId?: string, label?: string) => void,
+    onSelectValue: () => void,
+) => {
+    const id = selectedId == selection.id ? undefined : selection.id;
+    const label = labelOfSelectedId == selection.label ? undefined : selection.label;
+    onChange(true, id, undefined, undefined);
+    setSelectedId(id);
+    setLabelOfSelectedId(label);
+    if (id != null) onSelectValue();
 };
