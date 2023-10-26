eb3dba2b8241ebec58d74785e569f4aa568c7084
laila abjil <laila.abjil@keyconsulting.fr>
2023-10-26 07:17:03 +0200

    fix(datepicker): fix langue default

--- src/ui/components/Datepicker/Datepicker.tsx
+++ src/ui/components/Datepicker/Datepicker.tsx
@@ -39,7 +39,7 @@ const Datepicker = memo((props: DatepickerProps) => {
                     <label>{tipsLabel}&nbsp;?</label>
                 </Box>
             )}
-            <LocalizationProvider adapterLocale={"en"} dateAdapter={AdapterDayjs}>
+            <LocalizationProvider adapterLocale={"fr"} dateAdapter={AdapterDayjs}>
                 <DatePicker
                     key={id}
                     disabled={!componentSpecificProps?.modifiable}
