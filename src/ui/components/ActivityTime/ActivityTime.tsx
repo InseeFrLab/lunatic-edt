c300de9d56edb252b8f9d3c997764749d1c4f278
MailineN <nguyen.mailine@gmail.com>
2024-09-18 09:50:36 +0200

    fix: min time not being immutable when creating activity

--- src/ui/components/ActivityTime/ActivityTime.tsx
+++ src/ui/components/ActivityTime/ActivityTime.tsx
@@ -112,7 +112,7 @@ const ActivityTime = memo((props: ActivityTimeProps) => {
                 id={id}
                 value={startTime}
                 componentSpecificProps={componentSpecificProps}
-                minTime={startTime}
+                minTime={startTimeComputed.format(componentSpecificProps?.constants.FORMAT_TIME)}
             />
             {componentSpecificProps?.helpStep != null && (
                 <Box className={classes.imageHelpBox}>{componentSpecificProps?.helpImage}</Box>
